16157e61b5cec4f5618872cac49744f6d8431dfb
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-06-16 09:43:12 +0200

    changed tododata.json to taskdata.json, created GET tasks

--- todo.js
+++ todo.js
@@ -4,10 +4,32 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const taskData = JSON.parse(fs.readFileSync('taskdata.json', 'utf-8'));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use()
+app.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
 
-console.log('tihan');
+app.get('/tasks', (request, response) => {
+    const task_id = request.query.id;
+    if (task_id) {
+        const task = taskData.tasks.find((task) => String(task.id) === task_id);
+        if (task) {
+            response.status(200).send(task);
+        } else {
+            response.status(404).send('Task not found');
+        }
+    } else {
+        response.status(200).send(taskData);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Task App listening on port ${port}`);
+});
